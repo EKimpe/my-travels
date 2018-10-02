9ec1911236c96119cf8477166725d38d05a17653
Emma Kimpe <emmaemobaka@gmail.com>
2018-10-02 18:00:18 +0200

    adding new content

--- src/Travels.js
+++ src/Travels.js
@@ -16,6 +16,24 @@ const travels = [
     text3: "much kilometers",
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ4aGi2TlX-8VhLbfh3REm6uXXy890Sj_G_Tkbmw-QdTGTz4AQgw"
   },
+  {
+    text1: "Europe",
+    text2: "Danemark",
+    text3: "much kilometers",
+    image: "https://www.holidayguru.fr/wp-content/uploads/2017/08/Zuiderkerk-in-Amsterdam-iStock-528503566-2_titel.jpg"
+  },
+  {
+    text1: "Asia",
+    text2: "Japan",
+    text3: "much kilometers",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf_bajvzUkpsXZhdJ548XOC9z-JWNd79OWnPE1urvLrPcssBaAUg"
+  },
+  {
+    text1: "North America",
+    text2: "Canada",
+    text3: "much kilometers",
+    image: "http://www.voyagetips.com/wp-content/uploads/2018/01/yukon-canada.jpg"
+  },
 ];
 
 const Travels = () => (
